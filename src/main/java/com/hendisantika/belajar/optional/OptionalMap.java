3a10175de2d5cc41c86aa8ce3205ce3e743250af
hendisantika <hendisantika@yahoo.co.id>
2019-05-31 08:37:29 +0700

    #11 - Add OptionalMap

---
+++ src/main/java/com/hendisantika/belajar/optional/OptionalMap.java
@@ -0,0 +1,20 @@
+package com.hendisantika.belajar.optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : java-optional-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-31
+ * Time: 08:37
+ */
+public class OptionalMap {
+    public static void main(String[] args) {
+
+        UserDictionary userDictionary = new UserDictionary();
+        for (int i = 0; i < 3; i++) {
+            userDictionary.getUserByNumber(i).map(String::toUpperCase).ifPresent(name -> System.out.println("Name is " + name));
+        }
+    }
+}
