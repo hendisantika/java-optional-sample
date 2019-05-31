27658e5d3479ccacec15beeafa9e2e1b24ca5405
hendisantika <hendisantika@yahoo.co.id>
2019-05-31 08:33:18 +0700

    #5 - Remove .gradle folder & add NullCheck Class

---
+++ src/main/java/com/hendisantika/belajar/optional/NullCheck.java
@@ -0,0 +1,13 @@
+package com.hendisantika.belajar.optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : java-optional-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-05-31
+ * Time: 08:31
+ */
+public class NullCheck {
+}
