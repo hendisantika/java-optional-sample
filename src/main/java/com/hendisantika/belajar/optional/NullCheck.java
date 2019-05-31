daae66c75a148b242d703dcc13bf2173b3587c42
hendisantika <hendisantika@yahoo.co.id>
2019-05-31 08:33:58 +0700

    #6 - Modify NullCheck

--- src/main/java/com/hendisantika/belajar/optional/NullCheck.java
+++ src/main/java/com/hendisantika/belajar/optional/NullCheck.java
@@ -1,5 +1,7 @@
 package com.hendisantika.belajar.optional;
 
+import java.util.HashMap;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : java-optional-sample
@@ -10,4 +12,20 @@ package com.hendisantika.belajar.optional;
  * Time: 08:31
  */
 public class NullCheck {
+    public static void main(String[] args) throws Exception {
+
+        HashMap<Integer, String> stringMap = new HashMap<>();
+        stringMap.put(0, "Uzumaki Naruto");
+        stringMap.put(1, "Uchiha Sasuke");
+        stringMap.put(2, "Hatake Kakasi");
+
+        for (int i = 0; i < 4; i++) {
+            String name = stringMap.get(i);
+            if (name != null) {
+                System.out.println("Name " + i + " is " + name);
+            } else {
+                System.out.println("Name " + i + " is not found.");
+            }
+        }
+    }
 }
